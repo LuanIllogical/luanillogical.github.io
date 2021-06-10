5d37f891bb559f8a6b1a36d67497346055093cd5
Exitium <60335462+LuanIllogical@users.noreply.github.com>
2021-06-10 12:09:38 -0300

    Update stars.js

--- js/stars.js
+++ js/stars.js
@@ -14,7 +14,72 @@ window.onload = function() {
       }
       //Placeholder
       context.fillRect(0, 0, canvas.width, canvas.height);
+      var particles = {},
+          particleIndex = 0,
+          settings = {
+            density: 20,
+            particleSize: 10,
+          };
 
+      // Set up a function to create multiple particles
+      function Particle() {
+        // Establish starting positions and velocities
+        this.x = Math.floor(Math.random() * canvas.width);
+        this.y = Math.floor(Math.random() * canvas.height);
+
+        // Add new particle to the index
+        // Object used as it's simpler to manage that an array
+        particleIndex ++;
+        particles[particleIndex] = this;
+        this.id = particleIndex;
+        this.life = 0;
+        this.maxLife = 100;
+      }
+
+      // Some prototype methods for the particle's "draw" function
+      Particle.prototype.draw = function() {
+        // Age the particle
+        this.life++;
+
+        // If Particle is old, it goes in the chamber for renewal
+        if (this.life >= this.maxLife) {
+          delete particles[this.id];
+        }
+
+        // Create the shapes
+        context.clearRect(settings.leftWall, settings.groundLevel, canvas.width, canvas.height);
+        context.beginPath();
+        context.fillStyle="#ffffff";
+        //context.arc(this.x, this.y, settings.particleSize, 0, Math.PI*2, true); 
+        context.moveTo(this.x + 20, this.y);
+        context.lineTo(this.x, this.y + 20);
+        context.lineTo(this.x - 20, this.y);
+        context.lineTo(this.x, this.y - 20);
+        context.closePath();
+        context.fill();
+
+      }
+
+      setInterval(function() {
+        context.fillStyle = "rgba(10,10,10,0.8)";
+        context.fillRect(0, 0, canvas.width, canvas.height);
+
+        // Draw the particles
+        for (var i = 0; i < settings.density; i++) {
+          if (Math.random() > 0.97) {
+            // Introducing a random chance of creating a particle
+            // corresponding to an chance of 1 per second,
+            // per "density" value
+            new Particle();
+          }
+        }
+
+        for (var i in particles) {
+          particles[i].draw();
+        }
+      }, 30);
+
+/*
       var particles = {},
           particleIndex = 0;
 
@@ -68,5 +133,5 @@ window.onload = function() {
           particles[i].draw();
         }
       }, 333);
-
+*/
 };
