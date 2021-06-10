8ea24a6dbca4c487f9c1f71c9b509697e56800e7
Exitium <60335462+LuanIllogical@users.noreply.github.com>
2021-06-10 16:51:03 -0300

    Update stars.js

--- js/stars.js
+++ js/stars.js
@@ -51,9 +51,9 @@ window.onload = function() {
         context.beginPath();
         context.fillStyle="#ffffff";
         context.moveTo(this.x + (20 * (this.life / this.lifeMax)), this.y);
-        context.lineTo(this.x, this.y + (20 * (this.life / this.lifeMax)));
-        context.lineTo(this.x - (20 * (this.life / this.lifeMax)), this.y);
-        context.lineTo(this.x, this.y - (20 * (this.life / this.lifeMax)));   
+        context.lineTo(this.x, this.y + 20);
+        context.lineTo(this.x - 20, this.y);
+        context.lineTo(this.x, this.y - 20);   
         context.closePath(); 
         context.fill();
         this.oldScale = 2;
