1ea385fdd2d3d9a1b3f5130a6581ead1f7e9b717
Exitium <60335462+LuanIllogical@users.noreply.github.com>
2021-06-09 21:11:34 -0300

    Update stars.js

--- js/stars.js
+++ js/stars.js
@@ -1,23 +1,17 @@
 window.onload = function() {
-      // Initialise an empty canvas and place it on the page
       var canvas = document.createElement("canvas");
       var context = canvas.getContext("2d");
       canvas.width = window.innerWidth;
       canvas.height = window.innerHeight;
       document.body.appendChild(canvas);
-
-      // Fill the canvas with a black background to show where it is
+      
       context.fillRect(0, 0, canvas.width, canvas.height);
-
-      // Draw a square particle on the canvas
-      context.fillStyle = "white";
-      context.fillRect(300, 200, 10, 10);
-
-      // Draw a circle particle on the canvas
-      context.beginPath();
-      context.fillStyle = "white";
-      // After setting the fill style, draw an arc on the canvas
-      context.arc(500, 200, 10, 0, Math.PI*2, true); 
-      context.closePath();
-      context.fill();
+      
+      setInterval(function() {
+            context.beginPath();
+            context.fillStyle = "white";
+            context.arc(Math.floor(Math.random * window.innerWidth), Math.floor(Math.random * window.innerHeight), 10, 0, Math.PI * 2, true);
+            context.closePath();
+            context.fill();
+      }, 333);
 };
