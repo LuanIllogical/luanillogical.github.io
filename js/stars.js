272e768a34d7b673a9375a76f1dd0c0767e83646
Exitium <60335462+LuanIllogical@users.noreply.github.com>
2021-08-03 21:10:46 -0300

    Update stars.js

--- js/stars.js
+++ js/stars.js
@@ -1,5 +1,5 @@
 window.onload = function() {
-      var canvas = document.getElementById('thecanvas');;
+      var canvas = document.createElement('canvas');;
       var context = canvas.getContext("2d");
       var starSpawnTick = 0;
       canvas.width = window.innerWidth;
